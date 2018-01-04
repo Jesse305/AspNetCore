85440291bf223d719c5f5cc77583429649b2a8ff
Jesse Abreu <jesse.abreu@gdfnet.df.gov.br>
2018-01-04 12:50:59 -0200

    Completa CRUD de Categorias

--- Controllers/CategoriasController.cs
+++ Controllers/CategoriasController.cs
@@ -19,19 +19,67 @@ namespace Web.Controllers
             return View(categorias);
         } 
 
-        public IActionResult Form()
+        public IActionResult Form(int id)
         {
-            return View();
+            var categoria = _context.Categorias.FirstOrDefault(c => c.Id == id);
+            return View(categoria);
+        }
+
+        public bool NoDuplicateCad(string nome)
+        {
+            var categoriaCount = _context.Categorias.Where(c => c.Nome == nome).Count();
+
+            if(categoriaCount > 0){
+                return true;
+            }
+            return false;
+        }
+
+        public bool NoDuplicateEdit(int id, string nome){
+            var categoriaCount = _context.Categorias.Where(c => c.Id != id).Where(c => c.Nome == nome).Count();
+
+            if(categoriaCount > 0){
+                return true;
+            }
+            return false;
         }
         
         [HttpPost]
         public IActionResult Cadastro(Categoria categoria)
         {
-            _context.Categorias.Add(categoria);
-            _context.SaveChanges();
             
-            TempData["AlertaTipo"] = "success";
-            TempData["AlertaMsg"] = "Cadastro realizado com sucesso!";            
+            if(categoria.Id != 0){
+
+                if(this.NoDuplicateEdit(categoria.Id, categoria.Nome)){
+                    TempData["AlertaTipo"] = "error";
+                    TempData["AlertaMsg"] = "Categoria já possui cadastro!";
+
+                    return View("Form");
+
+                }else{
+                    _context.Categorias.Update(categoria);
+                    TempData["AlertaTipo"] = "success";
+                    TempData["AlertaMsg"] = "Cadastro alterado com sucesso!";
+
+                }                
+
+            }else{
+
+                if(this.NoDuplicateCad(categoria.Nome)){
+                    TempData["AlertaTipo"] = "error";
+                    TempData["AlertaMsg"] = "Categoria já possui cadastro!";
+
+                    return View("Form");
+
+                }else{
+                    _context.Categorias.Add(categoria);
+                    TempData["AlertaTipo"] = "success";
+                    TempData["AlertaMsg"] = "Cadastro realizado com sucesso!";
+                }
+                
+            }
+
+            _context.SaveChanges();                       
 
             return RedirectToAction("Index");
         }
